879715e9ef3ddcc42988b71ccb9514dd6b1a3c7f
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2023-10-04 19:34:14 -0300

    Chore: Created Feed Page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,7 +1,17 @@
+import FeedHeader from "@/components/FeedHeader"
+
 export default function Feed() {
   return (
-    <>
-      <h2 className="font-bold text-xl">Bem-vindo ao feed.</h2>
-    </>
-  );
+    <main className="flex min-h-screen flex-col justify-between bg-white">
+      <FeedHeader />
+      <div className="flex flex-wrap px-[48px] py-[71px] justify-between gap-y-4">
+        <div className="bg-[#D9D9D9] w-[360px] h-[718px] rounded-[10px]"></div>
+        <div className="bg-[#D9D9D9] w-[654px] h-[1256px] rounded-[10px]"></div>
+        <div className="flex flex-col gap-12">
+          <div className="bg-[#D9D9D9] w-[256px] h-[330px] rounded-[10px]"></div>
+          <div className="bg-[#D9D9D9] w-[256px] h-[330px] rounded-[10px]"></div>
+        </div>
+      </div>
+    </main>
+  )
 }
