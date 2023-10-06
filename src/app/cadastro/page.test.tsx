428d39ce98a26ea3f0748ff1fcded54759fac20d
José Otavio Carvalho <carvalho.jose@gmail.com>
2023-10-06 12:05:46 -0300

    feat: add cadastro page and test

---
+++ src/app/cadastro/page.test.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import Cadastro from "./page";
+
+const mockPush = jest.fn();
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({
+    push: mockPush,
+  })),
+}));
+
+describe("Component <Cadastro />", () => {
+  it("should render component", () => {
+    render(<Cadastro />);
+  });
+
+  it("should show error message when submitting blank form", async () => {
+    render(<Cadastro />);
+
+    const submitButtonElement = screen.getByText("Criar conta", {
+      selector: "button",
+    });
+
+    fireEvent.click(submitButtonElement);
+
+    await screen.findAllByText("Campo obrigatório");
+
+  });
+
+  it('should navigate to the "/feed" route when submitting correct credentials', async () => {
+    render(<Cadastro />);
+
+    const nameInput = screen.getByPlaceholderText(
+      "Insira seu nome completo"
+    );
+    const emailInput = screen.getByPlaceholderText(
+      "Ex: meu_email1234@exemplo.com"
+    );
+    const passwordInput = screen.getByPlaceholderText("Insira sua senha");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirme sua senha");
+    const submitButton = screen.getByText("Criar conta", {
+      selector: "button",
+    });
+
+    fireEvent.change(nameInput, { target: { value: "meu nome" } });
+    fireEvent.change(emailInput, { target: { value: "email@teste.com" } });
+    fireEvent.change(passwordInput, { target: { value: "123456" } });
+    fireEvent.change(confirmPasswordInput, { target: { value: "123456" } });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith("/feed");
+    });
+  });
+
+  it('should toggles password visibility when button is clicked', () => {
+    const { asFragment } = render(<Cadastro />);
+    const button = screen.getAllByTestId('toogleEye');
+  
+    expect(asFragment().querySelector('PiEyeSlashLight')).toBeDefined();
+  
+    fireEvent.click(button[0]);
+    expect(asFragment().querySelector('PiEyeLight')).toBeDefined();
+  
+    fireEvent.click(button[0]);
+    expect(asFragment().querySelector('PiEyeSlashLight')).toBeDefined();
+    
+    fireEvent.click(button[1]);
+    expect(asFragment().querySelector('PiEyeLight')).toBeDefined();
+  
+    fireEvent.click(button[1]);
+    expect(asFragment().querySelector('PiEyeSlashLight')).toBeDefined();
+  });
+});
---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,186 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FcGoogle } from "react-icons/fc";
+import { PiEyeLight, PiEyeSlashLight } from "react-icons/pi";
+import { authLogin } from "@/services/api";
+import { User } from "@/@types/User";
+import { Header } from "@/components/Header";
+
+export default function Cadastro() {
+  const router = useRouter();
+
+  const [showPasssword, setShowPassword] = useState({
+    password: false,
+    consfirmPassword: false
+  });
+
+  const registerValidationSchema = z.object({
+    name: z.string().min(1, "Campo obrigatório"),
+    email: z
+      .string()
+      .min(1, "Campo obrigatório")
+      .email("Formato e-mail inválido"),
+    password: z
+      .string()
+      .min(1, "Campo obrigatório")
+      .min(6, "Mínimo 6 caracteres"),
+    confirmPassword: z
+      .string()
+      .min(1, "Campo obrigatório")
+      .min(6, "Mínimo 6 caracteres"),
+  })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Senhas devem ser iguais",
+      path: ["confirmPassword"],
+  });
+
+  type RegisterValidationSchema = z.infer<typeof registerValidationSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterValidationSchema>({
+    resolver: zodResolver(registerValidationSchema),
+  });
+
+  async function userRegister(data: RegisterValidationSchema) {
+    const USER_EMAIL = "email@teste.com";
+    const USER_PASSWORD = "123456";
+
+    if (data.email !== USER_EMAIL || data.password !== USER_PASSWORD) {
+      console.log("E-mail ou senha incorretos.");
+      return;
+    }
+    router.push("/feed");
+  }
+
+  return (
+    <>
+      <Header />
+      <main className="mt-20 max-w-lg w-full mx-auto px-4">
+        <div className="flex flex-col items-center border border-zinc-300 rounded p-8">
+          <h2 className="text-2xl font-semibold text-green-color">
+            Criar nova conta
+          </h2>
+          <form
+            className="mt-5 w-full flex flex-col gap-6"
+            onSubmit={handleSubmit(userRegister)}
+          >
+            <div className="flex flex-col">
+              <label className="font-semibold">Nome completo:</label>
+              <input
+                type="text"
+                className={`font-regular border outline-none rounded border-zinc-500 py-2 px-3 ${
+                  errors.name && "login-error"
+                }`}
+                placeholder="Insira seu nome completo"
+                {...register("name")}
+              />
+              {errors.name && (
+                <span className="text-red-500 text-sm">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <label className="font-semibold">E-mail:</label>
+              <input
+                type="text"
+                className={`font-regular border outline-none rounded border-zinc-500 py-2 px-3 ${
+                  errors.email && "login-error"
+                }`}
+                placeholder="Ex: meu_email1234@exemplo.com"
+                {...register("email")}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <label className="font-semibold">Senha:</label>
+              <div
+                className={`border outline-none rounded border-zinc-500 flex items-center justify-between px-3 ${
+                  errors.password && "login-error"
+                }`}
+              >
+                <input
+                  type={showPasssword.password ? "text" : "password"}
+                  className={`w-full font-regular outline-none py-2 bg-transparent ${
+                    errors.password && "placeholder:text-red-500"
+                  }`}
+                  placeholder="Insira sua senha"
+                  {...register("password")}
+                />
+                <button data-testid="toogleEye" type="button" onClick={() => setShowPassword(prev => {return { ...prev, password: !prev.password }})}>
+                  {showPasssword.password ? <PiEyeLight /> : <PiEyeSlashLight />}
+                </button>
+              </div>
+              {errors.password && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <label className="font-semibold">Confirmar senha:</label>
+              <div
+                className={`border outline-none rounded border-zinc-500 flex items-center justify-between px-3 ${
+                  errors.confirmPassword && "login-error"
+                }`}
+              >
+                <input
+                  type={showPasssword.consfirmPassword ? "text" : "password"}
+                  className={`w-full font-regular outline-none py-2 bg-transparent ${
+                    errors.confirmPassword && "placeholder:text-red-500"
+                  }`}
+                  placeholder="Confirme sua senha"
+                  {...register("confirmPassword")}
+                />
+                <button data-testid="toogleEye" type="button" onClick={() => setShowPassword(prev => {return { ...prev, consfirmPassword: !prev.consfirmPassword }})}>
+                  {showPasssword.consfirmPassword ? <PiEyeLight /> : <PiEyeSlashLight />}
+                </button>
+              </div>
+              {errors.confirmPassword && (
+                <span className="text-red-500 text-sm">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="bg-green-color rounded w-full p-2 text-white"
+            >
+              Criar conta
+            </button>
+            <button
+              type="button"
+              className="rounded border border-zinc-500 text-black w-full p-2 flex items-center justify-center gap-2"
+            >
+              <FcGoogle size={20} />
+              Entrar com o Google
+            </button>
+          </form>
+
+          <span className="mt-24">
+            Já tem uma conta?{" "}
+            <Link
+              href="/"
+              className="text-green-color font-bold hover:underline"
+            >
+              Acessar conta
+            </Link>
+          </span>
+        </div>
+      </main>
+    </>
+  );
+}
